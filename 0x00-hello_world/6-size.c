adb4ee3558a36faee2e10896583f5bfad15d0a92
Nomad-cyber <morrismaingi2@gmail.com>
2023-02-16 13:11:34 -0800

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
-  GNU nano 4.8                                            6-size.c                                                      #include <stdio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -7,16 +6,16 @@
  */
 int main(void)
 {
-        int a;
-        long int b;
-        long long int c;
-        char d;
-        float f;
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
 
-        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-        return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
