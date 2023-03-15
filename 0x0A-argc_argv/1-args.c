70a240958592cc5ef95c4ed60f35fcb7282f12ef
Nomad-cyber <52195655+Nomad-cyber@users.noreply.github.com>
2023-03-15 21:45:31 +0300

    0x0A. C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
-printf("%i\n", argc - 1);
+	printf("%d\n", argc - 1);
 
-return (0);
+	return (0);
 }
